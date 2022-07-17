71dd867d19b65f272912491de15249a433db27cd
dumb cate <67748888+insc15@users.noreply.github.com>
2022-07-17 16:22:17 +0700

    Update index.jsx

--- pages/signin/index.jsx
+++ pages/signin/index.jsx
@@ -2,7 +2,8 @@ import LoginTemplate from "../../components/Templates/Login";
 import { cookiesParser } from "../../Utils/CookiesParser";
 
 export async function getServerSideProps(context) {
-    const cookies = cookiesParser(context.req.headers.cookie);
+    console.log(context)
+//     const cookies = cookiesParser(context.req.headers.cookie);
 //     if(cookies.lg){
 //         return{
 //             redirect: {
