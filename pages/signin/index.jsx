f02e58093288e139fd3163930d6400955ddcb191
Minh Hieu <ngohieu20031@gmail.com>
2022-07-17 16:36:06 +0700

    merge conflict

--- pages/signin/index.jsx
+++ pages/signin/index.jsx
@@ -2,18 +2,6 @@ import LoginTemplate from "../../components/Templates/Login";
 import { cookiesParser } from "../../Utils/CookiesParser";
 
 export async function getServerSideProps(context) {
-<<<<<<< HEAD
-    console.log(context)
-//     const cookies = cookiesParser(context.req.headers.cookie);
-//     if(cookies.lg){
-//         return{
-//             redirect: {
-//                 destination: '/',
-//                 permanent: true,
-//             }
-//         }
-//     }
-=======
     if(context.req.headers.cookie) 
     {
         const cookies = cookiesParser(context.req.headers.cookie);
@@ -33,8 +21,6 @@ export async function getServerSideProps(context) {
             }
         }
     }
-    
->>>>>>> acf0d81 (fix error when cookies not found)
 
     return {
         props: {},
